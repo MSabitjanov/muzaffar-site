bb8ec840eca1ee286a40247bca4b8fa8bb915477
Muzaffar <91193228+MSabitjanov@users.noreply.github.com>
2022-12-10 18:59:07 +0500

    Add new files

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    let active = document.getElementById('active')
+    if (active) {
+        active.style.color = '#0047FF';
+        active.style.fontWeight = 'bold';
+    }
+    
+});
+
+
